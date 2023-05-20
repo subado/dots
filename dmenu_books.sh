abc592e0b0de7052b6e4e5d51946f4f3fb3fba73
subado <subado.main@gmail.com>
2023-05-20 20:40:45 +0400

    Now All files showed

--- dmenu_books.sh
+++ dmenu_books.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-prefix="$HOME/books"
-book_files=( "$prefix"/*/*\.* )
-book_files+=( "$prefix"/*\.* )
-book_files=( "${book_files[@]#"$prefix"/}" )
+path="$HOME/books/"
+#book_files=$(find ${path} -type f ! -path "${path}.*" |  sed "s|^${path}||")
+IFS=$'\n' read -d '' -a book_files < <(find ${path} -type f ! -path "${path}.*")
+
+for i in "${!book_files[@]}";
+do
+  book_files[$i]="${book_files[$i]#$path}"
+done
 book=$(printf '%s\n' "${book_files[@]}" | dmenu -i -l 10 -fn "Hack Nerd Font-12")
 
 [[ -n $book ]] || exit
-zathura "$prefix/$book" &
+zathura "$path$book" &
 notify-send "${book#*/} opened."
