95ae225ad990b5948dcfcf6e568b7145531604f8
subado <subado.main@gmail.com>
2023-07-04 09:57:28 +0400

    Add utils.sh

---
+++ utils.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+PROGRAM="${0##*/}"
+GETOPT="getopt"
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+ITALIC=$(tput sitm)
+
+die() {
+	echo "$PROGRAM: $@" >&2
+	echo "Try '$PROGRAM --help' for more information."
+	exit 1
+}
+
+check_dir() {
+	[ ! -d "$1" ] && die "dir doesn't exist: $1"
+}
+
+contains() {
+	local value="$1"
+	shift
+	for i in "$@"; do
+		if [ "$i" == "$value" ]; then
+			return 0
+		fi
+	done
+	return 1
+}
+
+[ "$DEBUG" ] && set -x
