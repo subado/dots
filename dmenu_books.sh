c619f242f6e41cea31419c2efc78e6a0b16a275b
subado <subado.main@gmail.com>
2023-07-11 11:09:58 +0400

    Get rid of arrays and make script more flexible

--- dmenu_books.sh
+++ dmenu_books.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-path="$HOME/books/"
-#book_files=$(find ${path} -type f ! -path "${path}.*" |  sed "s|^${path}||")
-IFS=$'\n' read -d '' -a book_files < <(find ${path} -type f ! -path "${path}.*")
+path=${BOOKS_DIR:-"$HOME/books"}
+cd "$path" || exit 1
 
-for i in "${!book_files[@]}";
-do
-  book_files[$i]="${book_files[$i]#$path}"
-done
-book=$(printf '%s\n' "${book_files[@]}" | dmenu -i -l 10 -fn "Hack Nerd Font-12")
+book=$(find . -type f ! -path '*/\.*' | cut -c3- | dmenu -i -l 10 -fn "Hack Nerd Font-12")
 
-[[ -n $book ]] || exit
-zathura "$path$book" &
-notify-send "${book#*/} opened."
+[ -f "$path/$book" ] || exit
+xdg-open "$path/$book" &
+notify-send "${book##*/} opened."
