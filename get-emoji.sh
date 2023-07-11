450bd2c00d8936112f9ce49a21fc7353024971d9
subado <subado.main@gmail.com>
2023-07-12 00:48:29 +0400

    Add comments and to lower

--- get-emoji.sh
+++ get-emoji.sh
@@ -2,9 +2,12 @@
 
 FILE="${EMOJI_FILE:-emoji}"
 
-curl https://unicode.org/Public/emoji/15.0/emoji-test.txt -o "$FILE"
-sed -i -E '/^$|skin tone|^#/d' "$FILE"
-sed -i '/fully-qualified/!d' "$FILE"
-gawk -i inplace -F '#' '{print $2}' "$FILE"
-gawk -i inplace '{$0=gensub(/\s*\S+/,"",2)}1' "$FILE"
-sed -i -E 's/flag:|:|^ //g' "$FILE"
+curl https://unicode.org/Public/emoji/15.0/emoji-test.txt -o "$FILE" # Download file
+
+sed -i -E '/^$|skin tone|^#/d' "$FILE"      # Delete skin tone emoji (only yellow emoji will stay)
+sed -i '/fully-qualified/!d' "$FILE"        # Delete non fully-qualified emoji
+gawk -i inplace -F '#' '{print $2}' "$FILE" # Delete everything before #
+gawk -i inplace '{$2=""; print $0}' "$FILE" # Delete unicode version(third field)
+sed -i -E 's/flag:|://g' "$FILE"            # Delete 'flag:' and ':'
+sed -i 's/  / /g' "$FILE"                   # Replace double space with single
+sed -i 's/.*/\L&/g' "$FILE"                 # To lower
