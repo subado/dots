c887e7adf57d6b41127d49e5f22a7c1beb961137
subado <subado.main@gmail.com>
2023-07-11 13:54:45 +0400

    Use EMOJI_FILE instead of constant

--- dmenu-emoji.sh
+++ dmenu-emoji.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-grep -v "#" "$HOME"/.local/bin/scripts/emojis.txt | dmenu -i -l 20 -fn "Hack Nerd Font-15" | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+grep -v "#" "${EMOJI_FILE:-emoji}" | dmenu -i -l 20 -fn "Hack Nerd Font-15" | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
 clip="$(xclip -o -selection clipboard)"
 [ "$clip" != "" ] && notify-send "$clip copied to clipboard."
--- get-emoji.sh
+++ get-emoji.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FILE="emojis.txt"
+FILE="${EMOJI_FILE:-emoji}"
 
 curl https://unicode.org/Public/emoji/15.0/emoji-test.txt -o "$FILE"
 sed -i -E '/^$|skin tone|^#/d' "$FILE"
